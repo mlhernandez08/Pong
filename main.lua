bf838bd3727e09769fdb4390f5772387741381b6
mlhernandez08 <08mlhernandez@gmail.com>
2025-06-10 15:48:42 -0700

    updates up to lesson 4

--- main.lua
+++ main.lua
@@ -4,10 +4,14 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 push = require 'push'
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    math.randomseed(os.time())
 
     largeFont = love.graphics.newFont('PixeloidMono.ttf', 32)
     smallFont = love.graphics.newFont('PixeloidMono.ttf', 8)
@@ -21,12 +25,55 @@ function love.load()
         fullscreen = false
     })
     push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+
+    ballX = VIRTUAL_WIDTH /2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
+    gameState = 'start'
+
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
+
+            ballX = VIRTUAL_WIDTH /2 - 2
+            ballY = VIRTUAL_HEIGHT / 2 - 2
+        
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50)
+        end
     end
+end
+
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('s') then
+        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+    end
+
+    if love.keyboard.isDown('up') then
+        player2Y = math.max(0, player2Y + - PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('down') then
+        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+    end
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
+    end 
 end
 
 function love.draw()
@@ -38,13 +85,13 @@ function love.draw()
 
 
     -- paddle 1
-    love.graphics.rectangle('fill', 10, 10, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     -- paddle 2
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2Y, 5, 20)
 
     -- ball
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
 
     push.finish()
 end
